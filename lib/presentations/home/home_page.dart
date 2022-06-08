c57485bd9ece06f42c30d42d056b86a3230c9851
montassar98 <montassarselmi372@gmail.co>
2022-06-08 23:13:30 +0100

    change the home page

--- lib/presentations/home/home_page.dart
+++ lib/presentations/home/home_page.dart
@@ -5,8 +5,7 @@ import 'package:riadh_pfe/presentations/history/history_page.dart';
 import 'package:riadh_pfe/presentations/home/home_bloc.dart';
 import 'package:riadh_pfe/presentations/home/home_event.dart';
 import 'package:riadh_pfe/presentations/home/home_state.dart';
-
-import '../../api/constants.dart';
+import 'package:riadh_pfe/utils/extensions.dart';
 
 import 'dart:developer' as dev;
 
@@ -20,6 +19,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late HomeBloc _homeBloc;
+
   @override
   void initState() {
     super.initState();
@@ -36,14 +36,16 @@ class _MyHomePageState extends State<MyHomePage> {
         elevation: 0,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const SizedBox(height: 60,),
             BlocBuilder<HomeBloc, HomeState>(
               bloc: _homeBloc,
               builder: (context, state) {
-                if (state is LoadLastValueEvent) {
+                if (state is HomeLoadingState) {
+                  dev.log("show progress indicator");
                   return const CircularProgressIndicator();
                 }
                 if (state is HomeSuccessState) {
@@ -57,42 +59,51 @@ class _MyHomePageState extends State<MyHomePage> {
                 return Container();
               },
             ),
-            Center(
-                child: ClipRRect(
-              borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-              child: MaterialButton(
-                minWidth: 200.0,
-                height: 55,
-                color: Colors.green,
-                child: const Text('Refresh',
-                    style: TextStyle(fontSize: 16.0, color: Colors.white)),
-                onPressed: () {
-                  dev.log("on refresh clicked");
-                  _homeBloc.add(LoadLastValueEvent());
-                },
-              ),
-            )),
+            const Image(image: AssetImage("assets/images/uv_index.png")),
             const SizedBox(
               height: 20,
             ),
-            Center(
-                child: ClipRRect(
-              borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-              child: MaterialButton(
-                minWidth: 200.0,
-                height: 55,
-                color: Colors.black87,
-                child: const Text('History',
-                    style: TextStyle(fontSize: 16.0, color: Colors.white)),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const HistoryPage()),
-                  );
-                },
-              ),
-            )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                      child: MaterialButton(
+                        minWidth: 140.0,
+                        height: 55,
+                        color: Colors.green,
+                        child: const Text('Refresh',
+                            style: TextStyle(fontSize: 16.0, color: Colors.white)),
+                        onPressed: () {
+                          dev.log("on refresh clicked");
+                          _homeBloc.add(LoadLastValueEvent());
+                        },
+                      ),
+                    )),
+                const SizedBox(
+                  width: 20,
+                ),
+                Center(
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                      child: MaterialButton(
+                        minWidth: 140.0,
+                        height: 55,
+                        color: Colors.black87,
+                        child: const Text('History',
+                            style: TextStyle(fontSize: 16.0, color: Colors.white)),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HistoryPage()),
+                          );
+                        },
+                      ),
+                    )),
+              ],
+            )
           ],
         ),
       ),
@@ -105,7 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Image(image: AssetImage("assets/images/ic_thermometer.png")),
+            const Image(image: AssetImage("assets/images/uv_icon.png")),
             Padding(
               padding: const EdgeInsets.only(left: 10),
               child: Text(lastValue,
@@ -122,95 +133,56 @@ class _MyHomePageState extends State<MyHomePage> {
         buildCardsState(lastValue),
         const SizedBox(
           height: 100,
-        )
+        ),
       ],
     );
   }
 
   buildCardsState(String lastValue) {
     final double mLastValue = double.parse(lastValue);
-    if (mLastValue > Constants.dangerValue) {
-      //Danger zone
-      return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Card(
-            color: Colors.white.withAlpha(600),
-            child: Padding(
-              padding: const EdgeInsets.all(40),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset("assets/images/led_green.png"),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text('Safe zone',
-                      style: TextStyle(color: Colors.green, fontSize: 20))
-                ],
-              ),
-            ),
-          ),
-          Card(
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(40),
-              child: Column(
-                children: [
-                  Image.asset("assets/images/led_red.png"),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    'Danger zone',
-                    style: TextStyle(color: Colors.redAccent, fontSize: 20),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      );
-    } else {
-      return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Card(
-            child: Padding(
-              padding: const EdgeInsets.all(40),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset("assets/images/led_green.png"),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text('Safe zone',
-                      style: TextStyle(color: Colors.green, fontSize: 20))
-                ],
-              ),
-            ),
-          ),
-          Card(
-            color: Colors.white.withAlpha(600),
-            child: Padding(
-              padding: const EdgeInsets.all(40),
-              child: Column(
-                children: [
-                  Image.asset("assets/images/led_red.png"),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    'Danger zone',
-                    style: TextStyle(color: Colors.redAccent, fontSize: 20),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      );
+    dev.log(mLastValue.toString());
+    String color = "#129f47";
+    String title = "Low";
+    if (mLastValue >= 3.0 && mLastValue < 6.0) {
+      //Medium level
+      dev.log("medium level");
+      color = "#fbcc3e";
+      title = "Medium";
+    } else if (mLastValue >= 6.0 && mLastValue < 8.0) {
+      //high level
+      dev.log("high level");
+      color = "#f38304";
+      title = "High";
+    } else if (mLastValue >= 8.0 && mLastValue < 11.0) {
+      //very high level
+      dev.log("very high level");
+      color = "#e44c24";
+      title = "Very High";
+    } else if (mLastValue >= 11.0) {
+      //extremely high level
+      color = "#8b63a3";
+      title = "Extremely high";
     }
+
+    return Center(
+      child: Column(
+        children: [
+          const Text("current UV state:",
+              style: TextStyle(fontSize: 18.0, color: Colors.black)),
+          const SizedBox(height: 10),
+          ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+            child: MaterialButton(
+              minWidth: 200.0,
+              height: 55,
+              color: color.toColor(),
+              child: Text(title,
+                  style: const TextStyle(fontSize: 16.0, color: Colors.white)),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
